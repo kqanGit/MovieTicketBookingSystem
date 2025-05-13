9e3b32d969fbba3eb9cb10c72c827271fa19fea6
bhvphuc <buuhuynhvinhphuc@gmail.com>
2025-05-13 12:05:39 +0700

    adding user repo SQL to fetch data(pending)

---
+++ source/repository/AuthenticationRepositorySQL.cpp
@@ -0,0 +1,21 @@
+#include "AuthenticationRepositorySQL.h"
+#include <stdexcept>
+
+void AuthenticationRepositorySQL::addUser(const AccountInformation& info) {
+    std::string sql = "INSERT INTO users (username, password, phoneNumber, gmail, role) VALUES (?, ?, ?, ?, ?)";
+    dbConn->executeNonQuery(sql, {info.userName, info.password, info.phoneNumber, info.gmail, info.role});
+}
+
+AccountInformation AuthenticationRepositorySQL::getUserByUserName(const std::string& username, const std::string& password) {
+    std::string sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+    auto results = dbConn->executeQuery(sql, {username, password});
+    if (results.empty()) throw std::runtime_error("Invalid username or password");
+
+    AccountInformation info;
+    info.userName = results[0]["username"];
+    info.password = results[0]["password"];
+    info.phoneNumber = results[0]["phoneNumber"];
+    info.gmail = results[0]["gmail"];
+    info.role = results[0]["role"];
+    return info;
+}
---
+++ source/repository/IAuthenticationRepository.h
@@ -0,0 +1,15 @@
+#ifndef IAUTHENTICATIONREPOSITORY_H
+#define IAUTHENTICATIONREPOSITORY_H
+
+#include <string>
+#include <optional>
+#include "AccountInformation.h"
+
+class IAuthenticationRepository {
+public:
+    virtual void addUser(const AccountInformation& account) = 0;
+    virtual AccountInformation getUserByUserName(const std::string& username, const std::string& password) = 0;
+    virtual ~IAuthenticationRepository() = default;
+};
+
+#endif
