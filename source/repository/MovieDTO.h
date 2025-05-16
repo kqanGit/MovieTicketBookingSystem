782aff54d0c0d40e3241e33f6c332b264c719020
tthphat <truongthanhphat1103@gmail.com>
2025-05-17 00:06:07 +0700

    using namespace std;

--- source/repository/MovieDTO.h
+++ source/repository/MovieDTO.h
@@ -1,17 +1,26 @@
 #ifndef MOVIEDTO_H
 #define MOVIEDTO_H
-
+#include <iostream>
+#include <memory>       
 #include <string>
 using namespace std;
 
 class MovieDTO {
 public:
     int id;
-    string title;
-    string genre;
+    std::string title;
+    std::string genre;
     float rating;
-
-    MovieDTO(int id, const string& title, const string& genre, float rating);
+    
+    // Thêm constructor mặc định để thuận tiện
+    MovieDTO() : id(0), rating(0.0f) {}
+    MovieDTO(int id, const std::string& title, const std::string& genre, float rating);
+    
+    // Thêm phương thức so sánh để sử dụng trong tests
+    bool operator==(const MovieDTO& other) const {
+        return id == other.id && title == other.title 
+            && genre == other.genre && rating == other.rating;
+    }
 };
 
 #endif // MOVIEDTO_H
