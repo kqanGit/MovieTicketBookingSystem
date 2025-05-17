4a2f3567e56f2a357ef4a9e4156a0d78285e0ae9
tthphat <truongthanhphat1103@gmail.com>
2025-05-17 08:39:34 +0700

    Update MovieDTO no using namespace std

--- source/repository/MovieDTO.cpp
+++ source/repository/MovieDTO.cpp
@@ -1,4 +1,4 @@
 #include "MovieDTO.h"
 
-MovieDTO::MovieDTO(int id, const string& title, const string& genre, float rating)
+MovieDTO::MovieDTO(int id, const std::string& title, const std::string& genre, float rating)
     : id(id), title(title), genre(genre), rating(rating) {}
--- source/repository/MovieDTO.h
+++ source/repository/MovieDTO.h
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <memory>       
 #include <string>
-using namespace std;
+
 
 class MovieDTO {
 public:
